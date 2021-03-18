23b7887ccdf6cf3ea89f88f803584d40bd32043c
Bruno Brasolin <50347981+BrunoBrasolin@users.noreply.github.com>
2021-03-18 12:54:38 -0300

    começando responsividade

---
+++ script.js
@@ -0,0 +1,33 @@
+jQuery(".clientes-atendidos ul").slick({
+  arrows: false,
+  dots: false,
+  slidesToShow: 5,
+  autoplay: true,
+
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 4,
+      },
+    },
+    {
+      breakpoint: 850,
+      settings: {
+        slidesToShow: 3,
+      },
+    },
+    {
+      breakpoint: 767,
+      settings: {
+        slidesToShow: 2,
+      },
+    },
+    {
+      breakpoint: 650,
+      settings: {
+        slidesToShow: 1,
+      },
+    },
+  ],
+});
