b0a6faf0d4d44e30bdf6421b08f98edf9d1979ed
Bruno Brasolin <50347981+BrunoBrasolin@users.noreply.github.com>
2021-03-31 11:02:41 -0300

    mais vendidos slider

--- script.js
+++ script.js
@@ -100,6 +100,42 @@ $(document).ready(function () {
       },
     ],
   });
+  
+  $(".mais-vendidos-slider").slick({
+    arrows: true,
+    dots: false,
+    slidesToShow: 5,
+    autoplay: true,
+    prevArrow: '<svg class="arrow-left" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M16.67 0l2.83 2.829-9.339 9.175 9.339 9.167-2.83 2.829-12.17-11.996z"/></svg>',
+    nextArrow: '<svg class="arrow-right" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M5 3l3.057-3 11.943 12-11.943 12-3.057-3 9-9z"/></svg>',
+
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 4,
+        },
+      },
+      {
+        breakpoint: 850,
+        settings: {
+          slidesToShow: 3,
+        },
+      },
+      {
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 650,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
+  });
 
   $(".blockquote-slider").slick({
     arrows: false,
